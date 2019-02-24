3040c27a109bf4451394144e36b47654d69e68e3
dharmendra chouhan <dharmendra.falna@gmail.com>
2019-02-24 16:50:47 +0530

    filter

--- ulbstoremain/admin.py
+++ ulbstoremain/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Department,Financialyear
 
-admin.site.register(Department)
+@admin.register(Department)
+class PostDepartment(admin.ModelAdmin):
+    list_display = ('depcode','depdescription','status')
+    list_filter = ('depdescription', 'status')
+    search_fields = ('depcode', 'depdescription')
+    ordering = ('status', 'depcode')
+    
+
+
+
 admin.site.register(Financialyear)
